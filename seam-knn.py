ed13eadcb7989dd3fa231c04dc008a4cb9bdbc91
OliviaRuston <ogr20@bath.ac.uk>
2021-01-26 15:32:50 +0000

    Changed values to to_numpy()

--- seam-knn.py
+++ seam-knn.py
@@ -7,14 +7,18 @@ from sklearn.metrics import classification_report, confusion_matrix
 from sklearn.model_selection import cross_val_score
 from sklearn import metrics
 
+# Read in csv file
 seam = pd.read_csv('sample-torso-6.csv', encoding="utf-8", header=0)
 
+# Set feature and target
 target = seam.columns[-1]
 X = seam.drop(target, axis=1)
 y = seam.drop(X, axis=1)
 
+# Split data into test and train
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1, shuffle=1)
 
+# Scale data
 scaler = StandardScaler()
 scaler.fit(X_train)
 
@@ -27,11 +31,12 @@ k_value = []
 # Calculating prediction accuracy for K values between 1 and 40
 for i in range(1, 40):
     knn = KNeighborsClassifier(n_neighbors=i)
-    knn.fit(X_train, y_train.values.ravel())
+    knn.fit(X_train, y_train.to_numpy().ravel())
     pred_i = knn.predict(X_test)
     prediction_accuracy.append(metrics.accuracy_score(y_test, pred_i))
     k_value.append(i)
 
+# Plot K values vs Accuracy Prediction
 # plt.figure(figsize=(12, 6))
 # plt.plot(range(1, 40), prediction_accuracy, color='red', linestyle='dashed', marker='o',
 #          markerfacecolor='blue', markersize=10)
@@ -48,7 +53,7 @@ bestest_k_dudes = [item[1] for item in doof]
 
 for i in bestest_k_dudes:
     classifier = KNeighborsClassifier(n_neighbors=i)
-    classifier.fit(X_train, y_train.values.ravel())
+    classifier.fit(X_train, y_train.to_numpy().ravel())
 
     y_pred = classifier.predict(X_test)
 
